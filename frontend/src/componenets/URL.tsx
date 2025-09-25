b5f325599f9241ebdc686c6f7179f47b0a6b11ef
Yukta <yuktamaster07@gmail.com>
2025-09-25 19:22:35 +0530

    added vercel

--- frontend/src/componenets/URL.tsx
+++ frontend/src/componenets/URL.tsx
@@ -1,4 +1,3 @@
-//export const PRO_URL =   `http://127.0.0.1:8787`
-//export const PRO_URL =`https://backend.yuktamaster07.workers.dev`
+
 
 export const PRO_URL =`https://blogging-website-silk.vercel.app/`
