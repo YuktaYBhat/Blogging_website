b27856916403535eb662e56cb3a150ccc4ca2204
Yukta <yuktamaster07@gmail.com>
2025-09-26 20:15:09 +0530

    added backend

--- frontend/src/componenets/URL.tsx
+++ frontend/src/componenets/URL.tsx
@@ -1,3 +1,5 @@
 
 
-export const PRO_URL =`https://blogging-website-silk.vercel.app/`
+//export const PRO_URL =`https://blogging-website-silk.vercel.app/`
+
+export const PRO_URL =`backend.yuktamaster07.workers.dev`
